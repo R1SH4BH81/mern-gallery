3d622875eb55c4e54c212f6a926d28be97645323
Rishabh Mishra <rishabhmishra.81e@gmail.com>
2025-08-11 19:56:16 +0530

    models added

---
+++ server/models/file.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const fileSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    filename: {
+      type: String,
+      required: true,
+    },
+    fileType: {
+      type: String,
+      enum: ["video", "images"],
+      required: "true",
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("file", fileSchema);
